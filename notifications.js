38a0c99c4c7eae9b58a34f692e696150bd3884c6
riieezyy <reesharout14@gmail.com>
2026-08-12 20:32:25 +0530

    Rename notifications.js to js/notifications.js

--- notifications.js
+++
@@ -1,1 +0,0 @@
-export async function requestNotifications(){if('Notification' in window)return Notification.requestPermission()} export function notify(title,body){if(Notification?.permission==='granted')new Notification(title,{body,icon:'assets/logo/mimori-logo.png'})}
